bf2f13a4f2e81da0c63e4d1f226556c79ec5c669
Li Roman <lee695.13v2@gmail.com>
2018-09-17 15:10:37 +0600

    init project

---
+++ app/src/main/java/com/li695/amaze/Splash.kt
@@ -0,0 +1,12 @@
+package com.li695.amaze
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class Splash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
