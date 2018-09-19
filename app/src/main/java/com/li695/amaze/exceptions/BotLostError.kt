0a8c6f34dfe9a91d7ae9db7f4edda6ded31b7382
Li Roman <lee695.13v2@gmail.com>
2018-09-19 15:00:01 +0600

    Added a bot for the passage of the maze
    Took 32 minutes

---
+++ app/src/main/java/com/li695/amaze/exceptions/BotLostError.kt
@@ -0,0 +1,3 @@
+package com.li695.amaze.exceptions
+
+class BotLostError : Exception("Bot got lost in the maze")
---
+++ app/src/main/java/com/li695/amaze/exceptions/DeadEndError.kt
@@ -0,0 +1,3 @@
+package com.li695.amaze.exceptions
+
+class DeadEndError : Exception("Dead end of maze error")
